355f762a9c953fdb39a7526fae3385b85fbd4a4b
Steven <ssebexen@gmail.com>
2024-06-28 16:54:14 -0400

    More restrictive file perms

--- mkOutDir.go
+++ mkOutDir.go
@@ -9,7 +9,7 @@ func mkOutDir() {
 	_, err := os.Stat("out")
 
 	if os.IsNotExist(err) {
-		err := os.Mkdir("out", 0777)
+		err := os.Mkdir("out", 0755)
 		if err != nil {
 			fmt.Printf("Unable to create output directory: %s\n", err)
 		}
