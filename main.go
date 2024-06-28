1a0ec8ed8147a2b1c068e85a35ad8041eaabb0d1
Steven <ssebexen@gmail.com>
2024-06-28 12:15:28 -0400

    Neatened up logic.

--- main.go
+++ main.go
@@ -19,25 +19,27 @@ func fetchHtml(url string) *html.Node {
 	}
 	defer res.Body.Close()
 
-	// Use Content-Length if available, otherwise use a buffer to determine length
-	r := res.Body
+	var doc *html.Node
+
+	// Use Content-Length if available, otherwise use a buffer to determine length.
+	// Then, parse the HTML.
 	if res.ContentLength != -1 {
 		fmt.Printf("Content length: %d\n", res.ContentLength)
+		doc, err = html.Parse(res.Body)
+		if err != nil {
+			fmt.Printf("Error parsing HTML: %s", err)
+		}
 	} else {
 		buffer, err := io.ReadAll(res.Body)
 		if err != nil {
 			fmt.Printf("Error reading content: %s\n", err)
 			return nil
 		}
-		r = io.NopCloser(bytes.NewReader(buffer))
 		fmt.Printf("Content length: %d\n", len(buffer))
-	}
-
-	// Parse body or buffer
-	doc, err := html.Parse(r)
-	if err != nil {
-		fmt.Printf("Error while parsing: %s\n", err)
-		return nil
+		doc, err = html.Parse(bytes.NewBuffer(buffer))
+		if err != nil {
+			fmt.Printf("Error parsing HTML: %s", err)
+		}
 	}
 
 	return doc
