321613fe4fe7c908455fb32d8536c2abf61ab9c3
Steven <ssebexen@gmail.com>
2024-06-28 12:06:01 -0400

    Use Content-Length header if available.

--- main.go
+++ main.go
@@ -10,6 +10,39 @@ import (
 	"golang.org/x/net/html"
 )
 
+func fetchHtml(url string) *html.Node {
+	// HTTP Get
+	res, err := http.Get(url)
+	if err != nil {
+		fmt.Printf("Unable to fetch url %s\n", url)
+		return nil
+	}
+	defer res.Body.Close()
+
+	// Use Content-Length if available, otherwise use a buffer to determine length
+	r := res.Body
+	if res.ContentLength != -1 {
+		fmt.Printf("Content length: %d\n", res.ContentLength)
+	} else {
+		buffer, err := io.ReadAll(res.Body)
+		if err != nil {
+			fmt.Printf("Error reading content: %s\n", err)
+			return nil
+		}
+		r = io.NopCloser(bytes.NewReader(buffer))
+		fmt.Printf("Content length: %d\n", len(buffer))
+	}
+
+	// Parse body or buffer
+	doc, err := html.Parse(r)
+	if err != nil {
+		fmt.Printf("Error while parsing: %s\n", err)
+		return nil
+	}
+
+	return doc
+}
+
 func main() {
 	if len(os.Args) <= 1 {
 		println("Please provide a url.")
@@ -17,24 +50,7 @@ func main() {
 	}
 
 	url := os.Args[1]
-	res, err := http.Get(url)
-	if err != nil {
-		fmt.Printf("Unable to fetch url %s\n", url)
-		os.Exit(1)
-	}
-	defer res.Body.Close()
+	doc := fetchHtml(url)
 
-	buffer, err := io.ReadAll(res.Body)
-	if err != nil {
-		fmt.Printf("Error reading content: %s\n", err)
-		os.Exit(1)
-	}
-	fmt.Printf("Content length: %d\n", len(buffer))
-
-	doc, err := html.Parse(bytes.NewReader(buffer))
-	if err != nil {
-		fmt.Printf("Error while parsing: %s\n", err)
-		os.Exit(1)
-	}
 	fmt.Printf("%#v\n", doc.FirstChild)
 }
